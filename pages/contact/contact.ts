058e6831a4ecd01642a630cbe4da562717c10adf
jonnathan77 <jonnathanfsouzacruz@gmail.com>
2022-03-24 13:31:24 -0300

    4 files modified

--- pages/contact/contact.ts
+++ pages/contact/contact.ts
@@ -6,9 +6,24 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'contact.html'
 })
 export class ContactPage {
+  private contatos: Array<Object> = [];
 
   constructor(public navCtrl: NavController) {
 
-  }
-
+  
+  this.contatos = [
+    {
+      "nome": "Ricardo",
+      "sobrenome": "Crivelli",
+      "telefone": "+55 14 99999-9999"
+    },
+    {
+      "nome": "João",
+      "sobrenome": "Silva",
+      "telefone": "+55 14 55555-5555"
+    }
+  ]
 }
+}
+
+
